586677de179a72ba4cfbe714521c41cb947becac
henryfernandes <henryfernandes@gmail.com>
2016-09-22 10:27:01 +0000

    added docker compose

---
+++ ansible/roles/jenkins/files/deploygoapp.sh
@@ -0,0 +1,14 @@
+rm -rf goapp
+
+git clone https://github.com/henryfernandes/goapp.git/
+
+cd goapp
+
+sudo docker run -itd --rm -v /home/jenkins/goapp/code:/go/src/helloapp -v /home/jenkins/goapp/deploy:/tmp/deploy gobuild sh buildgo.sh
+
+sudo /usr/local/bin/docker-compose stop
+sudo /usr/local/bin/docker-compose rm -f
+
+sudo /usr/local/bin/docker-compose up
+
+
