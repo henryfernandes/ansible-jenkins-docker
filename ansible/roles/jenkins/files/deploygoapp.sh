f52570f17375250f26b65b855a9d51141cca1a3d
henryfernandes <henryfernandes@gmail.com>
2016-09-24 10:38:43 +0000

    made permission changes

--- ansible/roles/jenkins/files/deploygoapp.sh
+++ ansible/roles/jenkins/files/deploygoapp.sh
@@ -4,11 +4,6 @@ git clone https://github.com/henryfernandes/goapp.git/
 
 cd goapp
 
-sudo docker run --rm -v /home/jenkins/goapp/code:/go/src/helloapp -v /home/jenkins/goapp/deploy:/tmp/deploy gobuild sh buildgo.sh
-
-sudo /usr/local/bin/docker-compose stop
-sudo /usr/local/bin/docker-compose rm -f
-
-sudo /usr/local/bin/docker-compose up
+ansible-playbook -i hosts -c local build.yml
 
 
